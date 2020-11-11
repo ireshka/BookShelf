eb5be9726220693a00d92a38141223a4df8be2ad
ireshka <ireshka@users.noreply.github.com>
2020-11-11 01:00:14 +0100

    Minor changes before redeploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,12 +4,10 @@ const middleDebug = require('debug')('app:middle');
 
 const  checkAuthenticated = async (req, res, next) => {
   const token = req.cookies.token || null;
-  // if (!token) return res.status(401).send('Access denied. No token provided');
   middleDebug('token:', token);
 
   try {
     if (!token) {
-      console.log('nie ma tokenu');
       res.locals.name = '';
       res.locals.isEditor = false;
       return next();
